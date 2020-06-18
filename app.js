d43dc07b24628917d4faa54e9205b50d26c083e2
Sérgio Paulo <zebedal@bitbucket.org>
2020-06-18 21:42:21 +0100

    Final submit

--- app.js
+++ app.js
@@ -26,9 +26,7 @@ this.addEventListener('DOMContentLoaded', () => {
         document.body.style.overflow = 'auto';
     });
 
-    //test code
-    const textString = document.querySelector('.textanim').textContent;
-    console.log([...textString])
+  
 
 
 });
